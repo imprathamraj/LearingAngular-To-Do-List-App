91fb5426b5eada7af4ecfa03ee0d82193fbefcc5
Pratham Raj <rjpratham04@gmail.com>
2025-07-31 16:42:10 +0530

    [Pratham Raj]. Add header

---
+++ src/app/header/header.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { App } from '../app';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+
+}
